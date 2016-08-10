b85d82f2616909e43be06c6bab7a2dad9bb23d00
zzyang13717 <danielzhang0088@gmail.com>
2016-08-10 23:15:40 +1200

    Add project files and folders

---
+++ js/musichaandler.js
@@ -0,0 +1,7 @@
+var myClientId = "b1faef3e67e73d4cfd22bd69007bd3f9";
+function initSC() {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
---
+++ js/musichaandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "b1faef3e67e73d4cfd22bd69007bd3f9";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
